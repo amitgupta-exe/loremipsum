2bd2101399f46540d02070255ac3245c40a41ba2
Amit Gupta <mail.amit85764@gmail.com>
2021-09-22 17:37:56 +0530

    Basic model of Lorem Ipsum, Random letters at random places. With Number of Words.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
+
+
+const App = () => {
+
+  const Alphabet = ["a", "b", "c", "d", " ", "e", "f", "g", "h", "i", " ", "j", "k", "l", "m", " ", "n", "o", "p", " ", "q", "r", "s", " ", "t", "u", "v", "w", " ", "x", "y", "z", " "];
+
+
+  const [count, setCount] = useState(100);
+  const [text, setText] = useState([]);
+
+
+
+  const generateLorem = (e) => {
+
+e.preventDefault();
+    var Text = [];
+
+    for (let i = 0; i < (count * 4); i++) {
+
+      Text[i] = Alphabet[Math.floor(Math.random() * Alphabet.length)]
+    }
+
+    console.log(Text);
+
+    setText(Text);
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <main className="w-100 h-100">
+      <div className="d-flex justify-content-center py-5">
+        <h1 className="">Lorem Ipsum Generator</h1>
+      </div>
+      <form className="d-flex justify-content-center py-2">
+        <input onChange={(e) => {setCount(e.target.value)}} value={count} type="number" className="w-10" />
+        <button onClick={generateLorem} className="btn-secondary">Generate</button>
+      </form>
+      <div className="text-center ">
+        <p>{text}</p>
+      </div>
+    </main>
+  )
 }
 
 export default App;
